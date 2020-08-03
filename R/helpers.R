1cd444fe0d17c9fb066ba86247ee1dfc7373f41d
Raphael <raphael.sonabend.15@ucl.ac.uk>
2020-08-03 18:39:28 +0100

    helper fix

--- R/helpers.R
+++ R/helpers.R
@@ -1,5 +1,5 @@
 toproper = function(str) {
-  first = toupper(substr(dist, 1, 1))
-  rest = tolower(substr(dist, 2, 100))
+  first = toupper(substr(str, 1, 1))
+  rest = tolower(substr(str, 2, 100))
   return(paste0(first, rest))
 }
